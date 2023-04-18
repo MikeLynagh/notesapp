14f0838e15dba315cd619f55cdfd6f9ee95f8896
MikeLynagh <mlynagh111@gmail.com>
2023-04-18 21:35:33 +0100

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    const footerStyle = {
+        color: "green",
+        fontStyle: "italic",
+        fontSize: 20
+    }
+
+    return(
+        <div style={footerStyle}>
+            <br />
+            <em>Note app, built by Michael Lynagh </em>
+        </div>
+    )
+}
+
+export default Footer
