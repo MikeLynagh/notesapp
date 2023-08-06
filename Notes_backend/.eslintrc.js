a7b18c61ffd53d5aca51d64e203d696a8c72d8f3
MikeLynagh <mlynagh111@gmail.com>
2023-08-06 12:34:25 +0100

    added testing

---
+++ Notes_backend/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es2021": true,
+        "jest": true,
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "no-console": 0,
+
+        "indent":[
+            "error",
+            4
+        ], 
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+    }
+}
